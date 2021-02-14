6e624543347ce051538961da9172eec5eebb9678
Kostas Papaioannou <kopapaioan@gmail.com>
2021-02-14 15:46:25 +0200

    Updated functions

--- functions.hs
+++ functions.hs
@@ -43,10 +43,6 @@ second (_, y, _) = y
 third :: (a, b, c) -> c
 third (_, _, z) = z
 
-head' :: [a] -> a
-head' [] = error "Can't call head on an empty list, dummy!"
-head' (x:_) = x
-
 tell :: (Show a) => [a] -> String
 tell [] = "The list is empty"
 tell (x:[]) = "The list has one element: " ++ show x
@@ -63,12 +59,44 @@ capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
 bmiTell :: (RealFloat a) => a -> a -> String  
 bmiTell weight height  
-    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"  
-    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
-    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"  
     | otherwise                 = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)  
 
 max' :: (Ord a) => a -> a -> a
 max' a b
        | a > b     = a
        | otherwise = b    
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b   = GT
+    | a == b  = EQ
+    | otherwise = LT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h = 
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r * 2
+    in sideArea + 2 * topArea
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi | (w,h) <- xs, let bmi = w / h ^ 2]
+
+head' :: [a] -> a
+head' xs = case xs of [] -> error "No head for empty lists!"
+                      (x:_) -> x
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ what xs
+    where what [] = "empty"
+          what [x] = "a singleton list"
+          what xs = "a longer list"
